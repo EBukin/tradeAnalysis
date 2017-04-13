b559ddbc4af42abf9dddbd1f2c18907c60b94a82
Eduard Bukin <edwardbukin@gmail.com>
2017-04-13 09:26:07 +0300

    Shiny progress

--- tradeApp/server.R
+++ tradeApp/server.R
@@ -14,6 +14,11 @@ library(tidyr)
 library(jsonlite)
 library(stringr)
 library(shiny)
+# devtools::install_github("EBukin/tradeAnalysis", ref = "pack")
+library(tradeAnalysis)
+# install.packages("../tradeAnalysis_0.0.4Q.tar.gz")
+# load("tradeApp/availability.rdata")
+load("availability.rdata")
 
 # ctAval <-
 #   jsonlite::fromJSON("http://comtrade.un.org/api/refs/da/bulk?parameters") %>%
@@ -26,33 +31,67 @@ library(shiny)
 #          ps = str_c(str_sub(ps, 1, 4), "-", str_sub(ps, 5, 6)))
 # 
 # ctAvalBP <- ctAval
-if(!file.exists("availability.rdata")) {
-  ctAval <-
-    jsonlite::fromJSON("http://comtrade.un.org/api/refs/da/bulk?parameters") %>%
-    tbl_df() %>%
-    mutate(
-      Year = as.integer(str_sub(ps, 1, 4)),
-      Month = as.integer(ifelse(str_length(ps) > 4, str_sub(ps, 5, 6), NA)),
-      ps = str_c(str_sub(ps, 1, 4), "-", str_sub(ps, 5, 6))
-    )
-  save(ctAval, file = "availability.rdata")
-} else {
-  load("availability.rdata")
-}
+# if(!file.exists("availability.rdata")) {
+#   ctAval <-
+#     jsonlite::fromJSON("http://comtrade.un.org/api/refs/da/bulk?parameters") %>%
+#     tbl_df() %>%
+#     mutate(
+#       Year = as.integer(str_sub(ps, 1, 4)),
+#       Month = as.integer(ifelse(str_length(ps) > 4, str_sub(ps, 5, 6), NA)),
+#       ps = str_c(str_sub(ps, 1, 4), "-", str_sub(ps, 5, 6))
+#     )
+#   save(ctAval, file = "availability.rdata")
+# } else {
+#   load("availability.rdata")
+# }
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  observeEvent(input$updtCT, { 
-    ctAval <-
-      jsonlite::fromJSON("http://comtrade.un.org/api/refs/da/bulk?parameters") %>%
-      tbl_df() %>%
-      mutate(Year = as.integer(str_sub(ps, 1, 4)),
-             Month = as.integer(ifelse(str_length(ps) > 4, str_sub(ps, 5, 6), NA)),
-             ps = str_c(str_sub(ps, 1, 4), "-", str_sub(ps, 5, 6)))
-    save(ctAval, file = "availability.rdata")
-    load("availability.rdata")
-    })
+  
+  # Selecting full list of countries
+  countriesFilter <- ctAval %>% select(r) %>% distinct() %>% join_lables() %>% filter(!is.na(r))
+  countriesFilter <- setNames(countriesFilter$r, countriesFilter$Reporter)
+  
+  # Rendering uiCountries selector 
+  output$uiCountries <- renderUI({
+    # if (length(countriesFilter) == 0) return()
+    selectInput("countries", "Countries:", countriesFilter, selectize = TRUE, multiple = TRUE, selected = "ALL")
+  })
+  
+  dataCountries <- reactive({
+    ctAval %>% 
+      filter(r %in% input$countries) %>% 
+      return()
+  })
+  
+  # Rendering uiPeriods 
+  output$uiPeriodicity <- renderUI({
+    frequencies <- unique(dataCountries()$freq)
+    frequencies <- setNames(frequencies, frequencies)
+    selectInput("periodicity", "Periodicity:", frequencies)
+  })
+  
+  # Rendering uiMerchant 
+  output$uiMerchant <- renderUI({
+    type <- unique(dataCountries()$type)
+    type <- setNames(type, type)
+    selectInput("merchandize", "Merchandize type:", type, selectize = TRUE)
+  })
+  
+  # Rendering uiMerchant 
+  output$uiCoding <- renderUI({
+    coding <- unique(dataCountries()$px)
+    coding <- setNames(coding, coding)
+    selectInput("system", "Coding system:", coding, selected = "HS", selectize = TRUE)
+  })
+  
+  # Rendering uiYear 
+  output$uiYear <- renderUI({
+    Years <- unique(dataCountries()$Year)
+    sliderInput("yearsRange", label = h3("Years range"), min = min(Years), max = max(Years),
+                value = c(max(Years) - 5, max(Years)), step = 1L)
+  })
   
   data <- 
     reactive({
@@ -60,52 +99,51 @@ shinyServer(function(input, output) {
       merchant <- as.character(input$merchandize)
       codingSystem <- as.character(input$system)
       years <- seq(input$yearsRange[1], input$yearsRange[2], 1)
-      countries <- as.character(input$countries)
-      months <- NA
-      if(any(is.null(countries))) {countries <- "ALL"}
       
       # Data filtering
       data <- 
-        ctAval %>% 
-        filter(freq == period,
-               type == merchant,
-               px == codingSystem,
-               Year %in% years)
-      
-      if(period != "ANNUAL") {
-        months <- seq(input$monthRange[1], input$monthRange[2], 1)
-        data <- data %>% filter(Month %in% months)
-      } 
-      
-      if("FSR" %in% countries) {
-        countries <- c(countries[!countries %in% "FSR"], 
-                       51, 31, 112, 268, 398, 417, 498, 643, 762, 795, 804, 860)
-      }
-      
-      if(any(countries != "ALL")) {
-        data <- data %>% filter(r %in% as.character(countries))
-      } else {
-        data <- data
-      }
-      
-      # Reshaping results 
-      data <-
-        data %>% 
-        select(r, ps) %>% 
-        mutate(Value = "X") %>% 
-        spread(ps, Value)
-      
-      # Returning the results
-      list(data = data, 
-           filters = list(period = period,
-                          merchant = merchant,
-                          codingSystem = codingSystem,
-                          years = years,
-                          months = months,
-                          countries = countries))
+       ctAval %>% 
+       filter(freq == period,
+              type == merchant,
+              px == codingSystem,
+              Year %in% years)
+      # 
+      # if(period != "ANNUAL") {
+      #   months <- seq(input$monthRange[1], input$monthRange[2], 1)
+      #   data <- data %>% filter(Month %in% months)
+      # } 
+      # 
+      # if("FSR" %in% countries) {
+      #   countries <- c(countries[!countries %in% "FSR"], 
+      #                  51, 31, 112, 268, 398, 417, 498, 643, 762, 795, 804, 860)
+      # }
+      # 
+      # if(any(countries != "ALL")) {
+      #   data <- data %>% filter(r %in% as.character(countries))
+      # } else {
+      #   data <- data
+      # }
+      # 
+      # # Reshaping results 
+      # data <-
+      #   data %>% 
+      #   select(r, ps) %>% 
+      #   mutate(Value = "X") %>% 
+      #   spread(ps, Value)
+      # 
+      # # Returning the results
+      # list(data = data, 
+      #      filters = list(period = period,
+      #                     merchant = merchant,
+      #                     codingSystem = codingSystem,
+      #                     years = years,
+      #                     months = months,
+      #                     countries = countries))
       })
   
   output$table <- renderTable({data()$data})
   output$filters <- renderPrint({data()$filters})
   
 })
+
+
--- tradeApp/ui.R
+++ tradeApp/ui.R
@@ -8,22 +8,17 @@
 #
 
 library(shiny)
-source("../R/getCTPartners.R")
-# source("../R/getFSR.R")
-source("../R/getCTTradeClass.R")
-# load("availability.rdata")
-load("parts.rdata")
 
 # Initializing all trade classification systems
-classifications <- getCTTradeClass()
-
-# Initialinsing all partners
-# partners <- c(setNames(getFSR()$Partner.Code, getFSR()$Partner),setNames(getFSR()$Partner.Code, rep("FSR", 12)))
-
-#setNames(getCTPartners()$Partner.Code, getCTPartners()$Partner)
-
-countriesFilter <- as.list(partners[1:12])
-countriesFilter$FSR <- "FSR" #as.vector(partners[13:24])
+# classifications <- getCTTradeClass()
+# 
+# # Initialinsing all partners
+# # partners <- c(setNames(getFSR()$Partner.Code, getFSR()$Partner),setNames(getFSR()$Partner.Code, rep("FSR", 12)))
+# 
+# #setNames(getCTPartners()$Partner.Code, getCTPartners()$Partner)
+# 
+# countriesFilter <- as.list(partners[1:12])
+# countriesFilter$FSR <- "FSR" #as.vector(partners[13:24])
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -33,22 +28,10 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      
-      # Action button for updating the COMTRADDE data avilability
-      # actionButton(inputId = "updtCT", label = "Update COMTRADE"),
-      # br(),
-      selectInput("periodicity", "Periodicity:",
-                  c("Annual" = "ANNUAL",
-                    "Mounthly" = "MONTHLY")),
-      selectInput("merchandize", "Merchandize type:", 
-                  c("Commodities" = "COMMODITIES", "Services" = "SERVICES"), selectize = TRUE),
-      selectInput("system", "Coding system:", classifications, selectize = TRUE),
-      selectInput("countries", "Countries:", countriesFilter, selectize = TRUE, multiple = TRUE, selected = "ALL"),
-      sliderInput("yearsRange", label = h3("Years range"), min = 2000L, max = 2020L,
-                  value = c(2012L, 2017L), step = 1L),
-      sliderInput("monthRange", label = h3("Month range"), min = 1L, max = 12L,
-                  value = c(1L, 12L), step = 1L)
-      
+      uiOutput("uiCountries"),
+      uiOutput("uiPeriodicity"),
+      uiOutput("uiCoding"),
+      uiOutput("uiYear")
     ),
     
     # Show a plot of the generated distribution
